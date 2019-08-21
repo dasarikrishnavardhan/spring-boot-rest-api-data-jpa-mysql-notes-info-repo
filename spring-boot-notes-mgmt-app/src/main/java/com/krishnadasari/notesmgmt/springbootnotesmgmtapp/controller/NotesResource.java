26c2cbf52348c560602ff53999ebe29f6529dac5
dasarikrishnavardhan <dasarikrishnavardhan@outlook.com>
2019-08-21 21:58:39 +0530

    Added code to delete notes

--- spring-boot-notes-mgmt-app/src/main/java/com/krishnadasari/notesmgmt/springbootnotesmgmtapp/controller/NotesResource.java
+++ spring-boot-notes-mgmt-app/src/main/java/com/krishnadasari/notesmgmt/springbootnotesmgmtapp/controller/NotesResource.java
@@ -6,6 +6,7 @@ package com.krishnadasari.notesmgmt.springbootnotesmgmtapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,15 @@ public class NotesResource
 	public NotesInfo updateNotes(@PathVariable("notesId") Integer notesId,@RequestBody NotesInfo notes)
 	{
 		return notesManagementService.updateNotes(notesId, notes);
+		
+	}
+	
+	@DeleteMapping(path = "/{notesId}")
+	public String removeNotes(@PathVariable("notesId") Integer notesId)
+	{
+		 notesManagementService.deleteNotes(notesId);
+		 
+		 return "Notes with Id : "+notesId+" Deleted Sucessfully";
 		
 	}
 
