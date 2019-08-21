2edf49e2c0164f8565a5cddf08d7f7fb60aa989a
dasarikrishnavardhan <dasarikrishnavardhan@outlook.com>
2019-08-21 20:09:36 +0530

    initial code commit for Note mgmt using spring boot spring data jpa and mysql

---
+++ spring-boot-notes-mgmt-app/src/main/java/com/krishnadasari/notesmgmt/springbootnotesmgmtapp/SpringBootNotesMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.krishnadasari.notesmgmt.springbootnotesmgmtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotesMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNotesMgmtAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-notes-mgmt-app/src/test/java/com/krishnadasari/notesmgmt/springbootnotesmgmtapp/SpringBootNotesMgmtAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krishnadasari.notesmgmt.springbootnotesmgmtapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootNotesMgmtAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
